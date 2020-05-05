d6ae0adf093fe66d4934c4160645128f32a25a9e
admin <zhangsan@2869616157.com>
2020-05-05 11:57:21 +0800

    第二此提价

--- git-test/src/cn/itcast/test/GitTest.java
+++ git-test/src/cn/itcast/test/GitTest.java
@@ -3,5 +3,8 @@ package cn.itcast.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好,实际");
+        System.out.println("你哈珀");
+        System.out.println("你哈珀");
+        System.out.println("你哈珀");
     }
 }
